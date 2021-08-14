7af2abc189efa38f3b08810f44cb75157045a194
Gareth Ng <garethcng@gmail.com>
2021-08-14 15:12:17 -0400

    Updates

---
+++ precache-manifest.1a6143c9b48c31e47ad16e0935c9f3e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bb5fd0fa59a63a2769d68140633ebf",
+    "url": "/cubeclock/index.html"
+  },
+  {
+    "revision": "4450c70c4ff45670a0b6",
+    "url": "/cubeclock/static/css/2.1192a77d.chunk.css"
+  },
+  {
+    "revision": "7851b4671a36645d405d",
+    "url": "/cubeclock/static/css/main.1eb4108a.chunk.css"
+  },
+  {
+    "revision": "4450c70c4ff45670a0b6",
+    "url": "/cubeclock/static/js/2.ded9bde3.chunk.js"
+  },
+  {
+    "revision": "b89c6e028c7ae0de5475b0003782bec7",
+    "url": "/cubeclock/static/js/2.ded9bde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7851b4671a36645d405d",
+    "url": "/cubeclock/static/js/main.42aa7638.chunk.js"
+  },
+  {
+    "revision": "68261d5f127f076bc4aa",
+    "url": "/cubeclock/static/js/runtime-main.c4d6d9f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.208a532caa02cbec563e8f2697bd889d.js"
+  "/cubeclock/precache-manifest.1a6143c9b48c31e47ad16e0935c9f3e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cubeclock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
